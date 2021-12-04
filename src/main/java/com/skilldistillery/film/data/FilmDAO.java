0732980496c2f108b44f399b018b11b8bdd7f363
Daniel Faulkner <danfaulkner0@gmail.com>
2021-12-04 08:41:13 -0700

    updated FilmDAO

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.film.data;
 
+import java.sql.SQLException;
+
+import com.skilldistillery.film.entities.Film;
+
 public interface FilmDAO {
-	// TODO: boolean createFilm(Film film) throws SQLException;
-	// TODO: Film getFilmById(int id) throws SQLException;
-	// TODO: Film getFilmByKeyword(String keyword) throws SQLException;
-	// TODO: boolean updateFilm(Film film) throws SQLException;
-	// TODO: boolean deleteFilmById(int id) throws SQLException;
+	public boolean createFilm(Film film) throws SQLException;
+	public Film getFilmById(int id) throws SQLException;
+	public Film getFilmByKeyword(String keyword) throws SQLException;
+	public boolean updateFilm(Film film) throws SQLException;
+	public boolean deleteFilmById(int id) throws SQLException;
 }
