4d269f4fd42521b23cdffafa5db75d82d056117c
Daniel Faulkner <danfaulkner0@gmail.com>
2021-12-04 07:44:30 -0700

    #3 Update Film Entity

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.film.data;
 
 public interface FilmDAO {
-	// TODO: implement CRUD
+	// TODO: boolean createFilm(Film film) throws SQLException;
+	// TODO: Film getFilmById(int id) throws SQLException;
+	// TODO: Film getFilmByKeyword(String keyword) throws SQLException;
+	// TODO: boolean updateFilm(Film film) throws SQLException;
+	// TODO: boolean deleteFilmById(int id) throws SQLException;
 }
