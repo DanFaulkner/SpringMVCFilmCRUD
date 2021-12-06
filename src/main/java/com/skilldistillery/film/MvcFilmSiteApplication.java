eb638150b5c9ce5943c9e7a0a3180e9621673135
Dane <me@dane.fyi>
2021-12-06 11:02:23 -0500

    Added WAR configs

--- src/main/java/com/skilldistillery/film/MvcFilmSiteApplication.java
+++ src/main/java/com/skilldistillery/film/MvcFilmSiteApplication.java
@@ -2,12 +2,19 @@ package com.skilldistillery.film;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MvcFilmSiteApplication {
+public class MvcFilmSiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcFilmSiteApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MvcFilmSiteApplication.class);
 	}
 
 }
